ec32825409288060c0b7e32b9ded54f448481d3d
Go Chiba <go.chiba@gmail.com>
2015-07-31 11:05:35 +0900

    Add error page

---
+++ html/product/product_ng.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>まるまるショップ</title>
+  </head>
+  <body>
+    商品が選択されていません<br>
+    <a href="product_list.php">戻る</a>
+  </body>
+</html>
+
