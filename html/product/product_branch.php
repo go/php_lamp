24a368eaa439ec2149e0473d688e25ff2adbdd6a
Go Chiba <go.chiba@gmail.com>
2015-07-31 11:03:23 +0900

    Add display and delete function

--- html/product/product_branch.php
+++ html/product/product_branch.php
@@ -1,4 +1,14 @@
 <?php
+  if (isset($_POST['disp']) == true) {
+    if (isset($_POST['procode']) == false) {
+      header('Location:product_ng.php');
+      exit();
+    }
+    $pro_code = $_POST['procode'];
+    header('Location:product_disp.php?procode=' . $pro_code);
+    exit();
+  }
+
   if (isset($_POST['add']) == true) {
     header('Location:product_add.php');
     exit();
@@ -11,6 +21,16 @@
     }
     $pro_code = $_POST['procode'];
     header('Location:product_edit.php?procode=' . $pro_code);
+    exit();
+  }
+
+  if (isset($_POST['del']) == true) {
+    if (isset($_POST['procode']) == false) {
+      header('Location:product_ng.php');
+      exit();
+    }
+    $pro_code = $_POST['procode'];
+    header('Location:product_delete.php?procode=' . $pro_code);
     exit();
   }
 ?>
