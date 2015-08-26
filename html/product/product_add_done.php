e833594cd8388bf48fb14de279617e04750d5bb3
Go Chiba <go.chiba@gmail.com>
2015-08-26 16:09:43 +0900

    Add support session check mechanism for product management module
      Add session check mechanism for each files if user has not authorized
      correctly, system will be show link for login page.

--- html/product/product_add_done.php
+++ html/product/product_add_done.php
@@ -1,3 +1,18 @@
+<?php
+  session_start();
+  session_regenerate_id(true);
+
+  if (isset($_SESSION['login']) == false) {
+    print 'ログインされていません<br>';
+    print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+    exit();
+  } else {
+    print $_SESSION['staff_name'];
+    print 'さんログイン中<br>';
+    print '<br>';
+  }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,37 +20,37 @@
     <title>まるまるショップ</title>
   </head>
   <body>
-  <?php
-    try {
-      $pro_name  = $_POST['name'];
-      $pro_price = $_POST['price'];
-      $pro_gazou_name = $_POST['gazou_name'];
-
-      $dsn      = 'mysql:dbname=Shop;host=db;charset=utf8';
-      $user     = 'shopadmin';
-      $password = 'adminadmin';
-      $dbh      = new PDO($dsn, $user, $password);
-
-      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      $sql    = 'INSERT INTO mst_product(name,price,gazou) VALUES(?,?,?)';
-      $stmt   = $dbh->prepare($sql);
-      $data[] = $pro_name;
-      $data[] = $pro_price;
-      $data[] = $pro_gazou_name;
-      $stmt->execute($data);
-
-      $dbh = null;
-
-      print $pro_name;
-      print ' を追加しました<br>';
-    } catch (Exception $e) {
-      print 'ただいま障害により大変ご迷惑をお掛けしています';
-      exit();
-    }
-  ?>
-
-  <a href="product_list.php">戻る</a>
+    <?php
+      try {
+        $pro_name  = $_POST['name'];
+        $pro_price = $_POST['price'];
+        $pro_gazou_name = $_POST['gazou_name'];
+  
+        $dsn      = 'mysql:dbname=Shop;host=db;charset=utf8';
+        $user     = 'shopadmin';
+        $password = 'adminadmin';
+        $dbh      = new PDO($dsn, $user, $password);
+  
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+        $sql    = 'INSERT INTO mst_product(name,price,gazou) VALUES(?,?,?)';
+        $stmt   = $dbh->prepare($sql);
+        $data[] = $pro_name;
+        $data[] = $pro_price;
+        $data[] = $pro_gazou_name;
+        $stmt->execute($data);
+  
+        $dbh = null;
+  
+        print $pro_name;
+        print ' を追加しました<br>';
+      } catch (Exception $e) {
+        print 'ただいま障害により大変ご迷惑をお掛けしています';
+        exit();
+      }
+    ?>
+  
+    <a href="product_list.php">戻る</a>
   </body>
 </html>
 
--- html/product/product_disp.php
+++ html/product/product_disp.php
@@ -1,3 +1,18 @@
+<?php
+  session_start();
+  session_regenerate_id(true);
+
+  if (isset($_SESSION['login']) == false) {
+    print 'ログインされていません<br>';
+    print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+    exit();
+  } else {
+    print $_SESSION['staff_name'];
+    print 'さんログイン中<br>';
+    print '<br>';
+  }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,56 +20,56 @@
     <title>まるまるショップ</title>
   </head>
   <body>
-  <?php
-    try {
-      $pro_code = $_GET['procode'];
-
-      $dsn      = 'mysql:dbname=Shop;host=db;charset=utf8';
-      $user     = 'shopadmin';
-      $password = 'adminadmin';
-      $dbh      = new PDO($dsn, $user, $password);
-
-      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      $sql    = 'SELECT code,name,price,gazou FROM mst_product WHERE code=?';
-      $stmt   = $dbh->prepare($sql);
-      $data[] = $pro_code;
-      $stmt->execute($data);
-
-      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-      $pro_name  = $rec['name'];
-      $pro_price = $rec['price'];
-      $pro_gazou_name = $rec['gazou'];
-
-      $dbh = null;
-
-      if ($pro_gazou_name == '') {
-        $disp_gazou = '';
-      } else {
-        $disp_gazou = '<img src="./gazou/'.$pro_gazou_name.'">'; 
-      }
-    } catch (Exception $e) {
-      print 'ただいま障害により大変ご迷惑をお掛けしています';
-      exit();
-    }
-  ?>
+    <?php
+      try {
+        $pro_code = $_GET['procode'];
   
-  商品情報参照<br>
-  <br>
-  商品コード<br>
-  <?php print $pro_code; ?>
-  <br>
-  商品名<br>
-  <?php print $pro_name; ?>
-  <br>
-  価格<br>
-  <?php print $pro_price; ?>円
-  <br>
-  <?php print $disp_gazou; ?>
-  <br>
-
-  <form>
-    <input type="button" onclick="history.back()" value="戻る">
-  </form>
+        $dsn      = 'mysql:dbname=Shop;host=db;charset=utf8';
+        $user     = 'shopadmin';
+        $password = 'adminadmin';
+        $dbh      = new PDO($dsn, $user, $password);
+  
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+        $sql    = 'SELECT code,name,price,gazou FROM mst_product WHERE code=?';
+        $stmt   = $dbh->prepare($sql);
+        $data[] = $pro_code;
+        $stmt->execute($data);
+  
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        $pro_name  = $rec['name'];
+        $pro_price = $rec['price'];
+        $pro_gazou_name = $rec['gazou'];
+  
+        $dbh = null;
+  
+        if ($pro_gazou_name == '') {
+          $disp_gazou = '';
+        } else {
+          $disp_gazou = '<img src="./gazou/'.$pro_gazou_name.'">'; 
+        }
+      } catch (Exception $e) {
+        print 'ただいま障害により大変ご迷惑をお掛けしています';
+        exit();
+      }
+    ?>
+    
+    商品情報参照<br>
+    <br>
+    商品コード<br>
+    <?php print $pro_code; ?>
+    <br>
+    商品名<br>
+    <?php print $pro_name; ?>
+    <br>
+    価格<br>
+    <?php print $pro_price; ?>円
+    <br>
+    <?php print $disp_gazou; ?>
+    <br>
+  
+    <form>
+      <input type="button" onclick="history.back()" value="戻る">
+    </form>
   </body>
 </html>
