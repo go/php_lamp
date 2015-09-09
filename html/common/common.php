83f351cfbc7e916244fdd7ec70925e9230a5024a
Go Chiba <go.chiba@gmail.com>
2015-09-09 17:31:41 +0900

    Removed unnecessary comment line
      Remove unnecessary comment line that used to check behavior of db
      connection.

--- html/common/common.php
+++ html/common/common.php
@@ -1,9 +1,7 @@
 <?php
   function connectDB($dbname, $hostname, $user, $password) {
-  #  $dsn      = "mysql:dbname=$dbname;host=$hostname;charset=utf8";
-  #  $dbh      = new PDO($dsn, $user, $password);
-    $dsn      = "mysql:dbname=Shop;host=db;charset=utf8";
-    $dbh      = new PDO($dsn, 'shopadmin', 'adminadmin');
+    $dsn      = "mysql:dbname=$dbname;host=$hostname;charset=utf8";
+    $dbh      = new PDO($dsn, $user, $password);
   
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
